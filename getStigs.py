a5c1e01d8d074eaa23a33b0c8ccddbc056c07647
Camden Moors <cmoors@mitre.org>
2022-07-19 10:38:56 -0400

    Add unique ID per benchmark

--- getStigs.py
+++ getStigs.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from difflib import SequenceMatcher
 import re
 import json
+import uuid
+import re
 
 url = "https://public.cyber.mil/stigs/downloads/"
 
@@ -15,11 +18,17 @@ headers = {
 
 req = requests.get(url, headers)
 soup = BeautifulSoup(req.content, 'html.parser')
-table = soup.find_all('table')[0] # Grab the first table
+table = soup.find_all('table')[0]  # Grab the first table
 
 row_marker = 0
 
 stigs = []
+knownURLs = []
+
+with open('stigs.json', 'r') as existingSTIGsFile:
+    existingSTIGs = json.load(existingSTIGsFile)
+    stigs.extend(existingSTIGs)
+    knownURLs.extend([stig['url'] for stig in existingSTIGs])
 
 def cleanText(inputText):
     return re.sub(' +', ' ', inputText.replace('\r', ' ').replace('\u200b', '').replace('\n', ' ').split('\t')[0].strip()).strip()
@@ -37,16 +46,32 @@ for row in table.find_all('tr'):
                 href = column.find('a')['href']
                 name = cleanText(column.get_text())
         if (href != "" and name != "" and size != "" and ('stig' in name.lower() or 'benchmark' in name.lower() or 'stig' in href.lower() or 'benchmark' in href.lower()) and "viewer" not in name.lower()):
-            # If we have a zip file
-            if (href.lower().endswith('.zip')):
-                stigs.append({
-                    'name': name,
-                    'href': href,
-                    'size': size
-                })
+            # Check if the similarity of the current name is similar to an existing name
+            newStig = True
+            hrefWithNoVersion = re.sub(r'V\d(\d?)(\d?)(\d?)(\d?)R\d(\d?)(\d?)(\d?)(\d?)', '', href)
+            for knownURL in knownURLs:
+                knownURLWithNoVersion = re.sub(r'V\d(\d?)(\d?)(\d?)(\d?)R\d(\d?)(\d?)(\d?)(\d?)', '', knownURL)
+                if SequenceMatcher(None, hrefWithNoVersion, knownURLWithNoVersion).ratio() > 0.99:
+                    print("Found a duplicate: " + href)
+                    print("Similar to: " + knownURL)
+                    newStig = False
+                    break
+            if newStig:
+                if (href.lower().endswith('.zip')):
+                    knownURLs.append(href)
+                    stigs.append({
+                        'id': str(uuid.uuid4()),
+                        'name': name,
+                        'url': href,
+                        'size': size
+                    })
+            else:
+                print("Existing STIG: " + name)
+    except KeyboardInterrupt:
+        exit()
     except:
-        print("Error parsing row: ")
-        print(row)
+        # Bad rows ignored, typicall they don't contain a download link
+        pass
 
 with open('stigs.json', 'w') as outfile:
     json.dump(stigs, outfile, indent=2)
