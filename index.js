501f005e1bce69d06ed85779b0d583f0a266f235
Camden Moors <cmoors@mitre.org>
2022-07-20 09:33:37 -0400

    Pass profile id via env

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const fs = require('fs');
 // Find current version
 const version = fs.readFileSync('/github/workspace/VERSION', 'utf-8');
 console.log(`Current version: ${version}`);
+
+console.log(process.env)
